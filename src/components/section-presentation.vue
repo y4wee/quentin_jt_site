1d4ccf536377f677911f9af06a567e0031ae9fc1
y4wee <quentin.jambert@gmail.com>
2021-11-23 14:56:24 +0100

    creation section presentation

---
+++ src/components/section-presentation.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div class="">
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: '',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
