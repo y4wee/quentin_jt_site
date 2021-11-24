d3d1d641cdce24493805c94b738975f28c77a634
y4wee <quentin.jambert@gmail.com>
2021-11-24 12:04:22 +0100

    ajout nouvelle font

--- src/App.vue
+++ src/App.vue
@@ -32,8 +32,18 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=M+PLUS+2:wght@100;200;300;400;500;600;700;800;900&family=Yuji+Boku&display=swap');
+@font-face {
+  font-family: 'Luthon Southard';
+  src:  url("./assets/font/Luthon-Southard-Serif.ttf") format("ttf"),
+        url("./assets/font/Luthon-Southard-Serif.otf") format("otf"),
+        url("./assets/font/Luthon-Southard-Script.otf") format("otf"),
+        url("./assets/font/Luthon-Southard-Script.ttf") format("ttf");
+}
 // variables sass
 $mainColor: darken($color: rgb(96, 96 , 96), $amount: 20%);
+$greenColor: rgb(86, 245 , 105);
+$purpleColor: #f556e2;
+$greyColor: rgb(216, 216 , 216);
 
 #app {
   font-family: 'M PLUS 2', sans-serif;
@@ -41,25 +51,27 @@ $mainColor: darken($color: rgb(96, 96 , 96), $amount: 20%);
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   width: 100%;
-  height: 100%;
+  height: auto;
   background-color: white;
   overflow: hidden;
   cursor: none;
 }
 body {
   width: 100vw;
-  height: 100vh;
+  height: auto;
   margin: 0;
   padding: 0;
   overflow: hidden;
 }
 header {
   position: fixed;
-  height: 140px;
+  height: 100px;
   width: 100%;
   display: flex;
   transform: translateY(-100%);
   border-bottom: solid 1px $mainColor;
+  background-color: white;
+  z-index: 5;
 }
 
 </style>
