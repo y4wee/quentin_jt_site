ad733fe88d990623b046574fc6c996ecb91d31ba
y4wee <quentin.jambert@gmail.com>
2021-11-25 16:56:27 +0100

    modification animation logo

--- src/components/logo.vue
+++ src/components/logo.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="Logo">
-    <div class="LogoLetter typewriter">
 
-      <span class="LogoLetterEach l1">&lt;&nbsp;</span>
-      <span class="LogoLetterEach l2">Y</span>
-      <span class="LogoLetterEach l3">a</span>
-      <span class="LogoLetterEach l4">w</span>
-      <span class="LogoLetterEach l5">e</span>
-      <span class="LogoLetterEach l6">e</span>
-      <span class="LogoLetterEach l7">.</span>
-      <span class="LogoLetterEach l8">d</span>
-      <span class="LogoLetterEach l9">e</span>
-      <span class="LogoLetterEach l10">v</span>
-      <span class="LogoLetterEach l11">&nbsp; /</span>
-      <span class="LogoLetterEach l12">></span>
-
-      <div class="LogoLetterTyp"></div>
-
+    <div class="LogoWords word1">
+      <span class="LogoWordsLetter l1">Y</span>
+      <span class="LogoWordsLetter l2">a</span>
+      <span class="LogoWordsLetter l3">w</span>
+      <span class="LogoWordsLetter l4">e</span>
+      <span class="LogoWordsLetter l5">e</span>
     </div>
+
+    <div class="LogoWords word2">
+      <span class="LogoWordsLetter l6">.</span>
+      <span class="LogoWordsLetter l7">d</span>
+      <span class="LogoWordsLetter l8">e</span>
+      <span class="LogoWordsLetter l9">v</span>
+    </div>
+
+    <div class="LogoTyp"></div>
+
   </div>
 </template>
 
@@ -35,47 +35,38 @@ export default {
         tl.delay(0.5)
 
         // barre typ animation
-        tl.to('.LogoLetterTyp', {height: "40px", top: "-15px", backgroundColor: "black", ease: "elastic", duration: 0.55})
-        tl.to('.LogoLetterTyp', {width: "20px", ease: "back",  left: "10px", duration: 0.25, onComplete: function() {
-          document.querySelector('.LogoLetterTyp').classList.add("active")
+        tl.to('.LogoTyp', {height: "40px", backgroundColor: "black", ease: "elastic", duration: 0.55})
+        tl.to('.LogoTyp', {width: "20px", ease: "back",  left: "10px", duration: 0.25, onComplete: function() {
+          document.querySelector('.LogoTyp').classList.add("active")
         }})
         
         // apparition des lettres 
-        tl.to('.l1', {display: "block", ease: "none", duration: 0.4, delay: 0.4})
-        tl.to('.l2', {display: "block", ease: "none", duration: 0.15})
-        tl.to('.l3', {display: "block", ease: "none", duration: 0.15})
-        tl.to('.l4', {display: "block", ease: "none", duration: 0.15})
-        tl.to('.l5', {display: "block", ease: "none", duration: 0.15})
-        tl.to('.l6', {display: "block", ease: "none", duration: 0.2})
-        tl.to('.l7', {display: "block", ease: "none", duration: 0.2})
-        tl.to('.l8', {display: "block", ease: "none", duration: 0.15})
-        tl.to('.l9', {display: "block", ease: "none", duration: 0.15})
-        tl.to('.l10', {display: "block", ease: "none", duration: 0.4})
-        tl.to('.l11', {display: "block", ease: "none", duration: 0.14})
-        tl.to('.l12', {display: "block", ease: "none", duration: 0.4})
+        tl.to('.l1', {display: "block", duration: 0.4, delay: 0.4})
+        tl.to('.l2', {display: "block", duration: 0.15})
+        tl.to('.l3', {display: "block", duration: 0.15})
+        tl.to('.l4', {display: "block", duration: 0.15})
+        tl.to('.l5', {display: "block", duration: 0.3})
+        tl.to('.l6', {display: "block", duration: 0.3})
+        tl.to('.l7', {display: "block", duration: 0.15})
+        tl.to('.l8', {display: "block", duration: 0.15})
+        tl.to('.l9', {display: "block", duration: 0.6})
         
-        tl.to('.l10', {display: "none", ease: "none", duration: 0.014})
-        tl.to('.l9', {display: "none", ease: "none", duration: 0.014})
-        tl.to('.l8', {display: "none", ease: "none", duration: 0.014})
-        tl.to('.l7', {display: "none", ease: "none", duration: 0.014})
-        tl.to('.l6', {display: "none", ease: "none", duration: 0.014})
-        tl.to('.l5', {display: "none", ease: "none", duration: 0.014})
-        tl.to('.l4', {display: "none", ease: "none", duration: 0.014})
-        tl.to('.l3', {display: "none", ease: "none", duration: 0.014})
-        tl.to('.l2', {color: "rgb(86, 245 , 105)", fontWeight: "bold", ease: "slow", duration: 0.4})
+        tl.to('.l5, .l4, .l3, .l2', {opacity: 0, ease: "sine.out", duration: 0.5})
+        tl.to('.word1', {width: "57.78px", ease: "sine.inOut", duration: 0.30}, "-=0.2")
+        tl.to('.l1', {color: "rgb(86, 245 , 105)", fontWeight: "bold", ease: "back.out", duration: 0.4}, "-=0.05")
 
         // disparition barre typ et logo
-        tl.to('.LogoLetterTyp', {opacity: "0", ease: "power3",duration: 0.2, onComplete: function() {
-          document.querySelector('.LogoLetterTyp').classList.remove("active");
-          gsap.to('.LogoLetterTyp', {display: "none"});
+        tl.to('.LogoTyp', {opacity: "0", ease: "power3",duration: 0.2, onComplete: function() {
+          document.querySelector('.LogoTyp').classList.remove("active");
+          gsap.to('.LogoTyp', {display: "none"});
         }})
         tl.to('.Logo', {opacity: 0, ease: "power3",duration: 0.3, onComplete: this.turnHeaderOn})
         tl.to('.Logo', {opacity: 1, duration: 0.3}, "=+0.3")
     },
     turnHeaderOn: function() {
       this.$store.commit('headerOn', true);
-      gsap.to('header', {transform: "translateY(-75%)", ease: "power3",duration: 0.3})
-      gsap.to('.Logo', {top: "26%",scale: 0.6, ease: "power3",duration: 0})
+      gsap.to('header', {transform: "translateY(0)", ease: "power3",duration: 0.3})
+      gsap.to('.Logo', {top: "0",scale: 0.6, ease: "power3",duration: 0})
     }
   },
   watch: {
@@ -102,21 +93,22 @@ $purpleColor: #f556e2;
 $greyColor: rgb(216, 216 , 216);
   .Logo {
     position: relative;
-    top: calc(50vh + 142px);
+    top: calc(50vh + 13px);
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
     height: 140px;
     transform-origin: center;
-    &Letter {
+    &Words {
       position: relative;
       display: flex;
-      justify-content: center;
+      justify-content: flex-start;
       align-items: flex-end;
       user-select: none;
       height: 140px;
-      &Each {
+      transform-origin: left;
+      &Letter {
         font-family: 'Luthon Southard', serif;
         font-size: 5em;
         color: black;
@@ -126,20 +118,23 @@ $greyColor: rgb(216, 216 , 216);
           transform: rotateY(180deg);
         }
       }
-      &Typ {
-        position: relative;
-        top: calc(50vh - 60px);
-        height: 100vh;
-        width: 2px;
-        border-radius: 3px;
-        background-color: darken($color: rgb(96, 96 , 96), $amount: 20%);
-        &.active {
-          animation: cursorTyping 0.25s 0.1s infinite alternate-reverse linear;
-        }
+      &.word1 {
+        overflow: hidden;
+        width: auto;
+      }
+    }
+    &Typ {
+      position: relative;
+      top: 35px;
+      height: 100vh;
+      width: 2px;
+      border-radius: 3px;
+      background-color: darken($color: rgb(96, 96 , 96), $amount: 20%);
+      &.active {
+        animation: cursorTyping 0.25s 0.1s infinite alternate-reverse linear;
       }
     }
   }
-
   @keyframes cursorTyping {
     0% {
       opacity: 0;
